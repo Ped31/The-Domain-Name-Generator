c612e97c40b8150d3a3b5073cdd7d0b6a8237924
Ped31 <pedrorivdom@gmail.com>
2020-11-06 16:57:31 +0000

    fixing previous mistake pushing js file

---
+++ app.js
@@ -0,0 +1,14 @@
+let pronoun = ['the','our'];
+let adj = ['great', 'big' ];
+let noun = ['jogger','racoon'];
+                        
+for(let i = 0; i <= pronoun.length - 1; i++){
+                            
+        for(let j = 0; j <= adj.length -1; j++ ){
+
+            for( let k = 0; k <= noun.length -1; k++)
+
+            console.log("www." + pronoun[i] +adj[j] + noun[k] + ".com");
+        }
+};
+  
